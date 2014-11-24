1f824705ec90a55b59d1c145c0e48327d069ec43
jonwchu <jwichu888@gmail.com>
2014-11-23 19:14:13 -0800

    Script folder

---
+++ Awesome/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMover : MonoBehaviour 
+{
+	public Transform ForwardRig;
+	public int Speed;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.transform.Translate(ForwardRig.forward * Speed * Time.deltaTime );
+	}
+}
